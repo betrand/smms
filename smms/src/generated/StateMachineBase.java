9f055fcaaa1f0f5ad29ce98e2f0ee7e69639bad7
bu_000 <betrandugorji@yahoo.co.uk>
2015-06-28 02:54:43 +0100

    project initializer first commit

--- smms/src/generated/StateMachineBase.java
+++ smms/src/generated/StateMachineBase.java
@@ -34,7 +34,9 @@ public abstract class StateMachineBase extends UIBuilder {
     public Container startApp(Resources res, String resPath, boolean loadTheme) {
         initVars();
         UIBuilder.registerCustomComponent("Form", com.codename1.ui.Form.class);
+        UIBuilder.registerCustomComponent("Button", com.codename1.ui.Button.class);
         UIBuilder.registerCustomComponent("Label", com.codename1.ui.Label.class);
+        UIBuilder.registerCustomComponent("TextField", com.codename1.ui.TextField.class);
         if(loadTheme) {
             if(res == null) {
                 try {
@@ -70,7 +72,9 @@ public abstract class StateMachineBase extends UIBuilder {
     public Container createWidget(Resources res, String resPath, boolean loadTheme) {
         initVars();
         UIBuilder.registerCustomComponent("Form", com.codename1.ui.Form.class);
+        UIBuilder.registerCustomComponent("Button", com.codename1.ui.Button.class);
         UIBuilder.registerCustomComponent("Label", com.codename1.ui.Label.class);
+        UIBuilder.registerCustomComponent("TextField", com.codename1.ui.TextField.class);
         if(loadTheme) {
             if(res == null) {
                 try {
@@ -108,6 +112,30 @@ public abstract class StateMachineBase extends UIBuilder {
         this(res, null, loadTheme);
     }
 
+    public com.codename1.ui.Button findButton1(Component root) {
+        return (com.codename1.ui.Button)findByName("Button1", root);
+    }
+
+    public com.codename1.ui.Button findButton1() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("Button1", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("Button1", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Button findButton(Component root) {
+        return (com.codename1.ui.Button)findByName("Button", root);
+    }
+
+    public com.codename1.ui.Button findButton() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("Button", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("Button", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
     public com.codename1.ui.Label findLabel(Component root) {
         return (com.codename1.ui.Label)findByName("Label", root);
     }
@@ -120,7 +148,64 @@ public abstract class StateMachineBase extends UIBuilder {
         return cmp;
     }
 
+    public com.codename1.ui.TextField findTextField(Component root) {
+        return (com.codename1.ui.TextField)findByName("TextField", root);
+    }
+
+    public com.codename1.ui.TextField findTextField() {
+        com.codename1.ui.TextField cmp = (com.codename1.ui.TextField)findByName("TextField", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.TextField)findByName("TextField", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public static final int COMMAND_RequestAccessSend = 3;
+    public static final int COMMAND_MainRequestAccess = 4;
+
+    protected boolean onRequestAccessSend() {
+        return false;
+    }
+
+    protected boolean onMainRequestAccess() {
+        return false;
+    }
+
+    protected void processCommand(ActionEvent ev, Command cmd) {
+        switch(cmd.getId()) {
+            case COMMAND_RequestAccessSend:
+                if(onRequestAccessSend()) {
+                    ev.consume();
+                    return;
+                }
+                break;
+
+            case COMMAND_MainRequestAccess:
+                if(onMainRequestAccess()) {
+                    ev.consume();
+                    return;
+                }
+                break;
+
+        }
+        if(ev.getComponent() != null) {
+            handleComponentAction(ev.getComponent(), ev);
+        }
+    }
+
     protected void exitForm(Form f) {
+        if("RequestAccess".equals(f.getName())) {
+            exitRequestAccess(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("LoginForm".equals(f.getName())) {
+            exitLoginForm(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
         if("Main".equals(f.getName())) {
             exitMain(f);
             aboutToShowThisContainer = null;
@@ -131,11 +216,31 @@ public abstract class StateMachineBase extends UIBuilder {
     }
 
 
+    protected void exitRequestAccess(Form f) {
+    }
+
+
+    protected void exitLoginForm(Form f) {
+    }
+
+
     protected void exitMain(Form f) {
     }
 
     protected void beforeShow(Form f) {
     aboutToShowThisContainer = f;
+        if("RequestAccess".equals(f.getName())) {
+            beforeRequestAccess(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("LoginForm".equals(f.getName())) {
+            beforeLoginForm(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
         if("Main".equals(f.getName())) {
             beforeMain(f);
             aboutToShowThisContainer = null;
@@ -146,11 +251,31 @@ public abstract class StateMachineBase extends UIBuilder {
     }
 
 
+    protected void beforeRequestAccess(Form f) {
+    }
+
+
+    protected void beforeLoginForm(Form f) {
+    }
+
+
     protected void beforeMain(Form f) {
     }
 
     protected void beforeShowContainer(Container c) {
         aboutToShowThisContainer = c;
+        if("RequestAccess".equals(c.getName())) {
+            beforeContainerRequestAccess(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("LoginForm".equals(c.getName())) {
+            beforeContainerLoginForm(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
         if("Main".equals(c.getName())) {
             beforeContainerMain(c);
             aboutToShowThisContainer = null;
@@ -161,10 +286,30 @@ public abstract class StateMachineBase extends UIBuilder {
     }
 
 
+    protected void beforeContainerRequestAccess(Container c) {
+    }
+
+
+    protected void beforeContainerLoginForm(Container c) {
+    }
+
+
     protected void beforeContainerMain(Container c) {
     }
 
     protected void postShow(Form f) {
+        if("RequestAccess".equals(f.getName())) {
+            postRequestAccess(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("LoginForm".equals(f.getName())) {
+            postLoginForm(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
         if("Main".equals(f.getName())) {
             postMain(f);
             aboutToShowThisContainer = null;
@@ -175,10 +320,30 @@ public abstract class StateMachineBase extends UIBuilder {
     }
 
 
+    protected void postRequestAccess(Form f) {
+    }
+
+
+    protected void postLoginForm(Form f) {
+    }
+
+
     protected void postMain(Form f) {
     }
 
     protected void postShowContainer(Container c) {
+        if("RequestAccess".equals(c.getName())) {
+            postContainerRequestAccess(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("LoginForm".equals(c.getName())) {
+            postContainerLoginForm(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
         if("Main".equals(c.getName())) {
             postContainerMain(c);
             aboutToShowThisContainer = null;
@@ -189,10 +354,30 @@ public abstract class StateMachineBase extends UIBuilder {
     }
 
 
+    protected void postContainerRequestAccess(Container c) {
+    }
+
+
+    protected void postContainerLoginForm(Container c) {
+    }
+
+
     protected void postContainerMain(Container c) {
     }
 
     protected void onCreateRoot(String rootName) {
+        if("RequestAccess".equals(rootName)) {
+            onCreateRequestAccess();
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("LoginForm".equals(rootName)) {
+            onCreateLoginForm();
+            aboutToShowThisContainer = null;
+            return;
+        }
+
         if("Main".equals(rootName)) {
             onCreateMain();
             aboutToShowThisContainer = null;
@@ -203,11 +388,31 @@ public abstract class StateMachineBase extends UIBuilder {
     }
 
 
+    protected void onCreateRequestAccess() {
+    }
+
+
+    protected void onCreateLoginForm() {
+    }
+
+
     protected void onCreateMain() {
     }
 
     protected Hashtable getFormState(Form f) {
         Hashtable h = super.getFormState(f);
+        if("RequestAccess".equals(f.getName())) {
+            getStateRequestAccess(f, h);
+            aboutToShowThisContainer = null;
+            return h;
+        }
+
+        if("LoginForm".equals(f.getName())) {
+            getStateLoginForm(f, h);
+            aboutToShowThisContainer = null;
+            return h;
+        }
+
         if("Main".equals(f.getName())) {
             getStateMain(f, h);
             aboutToShowThisContainer = null;
@@ -218,11 +423,31 @@ public abstract class StateMachineBase extends UIBuilder {
     }
 
 
+    protected void getStateRequestAccess(Form f, Hashtable h) {
+    }
+
+
+    protected void getStateLoginForm(Form f, Hashtable h) {
+    }
+
+
     protected void getStateMain(Form f, Hashtable h) {
     }
 
     protected void setFormState(Form f, Hashtable state) {
         super.setFormState(f, state);
+        if("RequestAccess".equals(f.getName())) {
+            setStateRequestAccess(f, state);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("LoginForm".equals(f.getName())) {
+            setStateLoginForm(f, state);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
         if("Main".equals(f.getName())) {
             setStateMain(f, state);
             aboutToShowThisContainer = null;
@@ -233,7 +458,51 @@ public abstract class StateMachineBase extends UIBuilder {
     }
 
 
+    protected void setStateRequestAccess(Form f, Hashtable state) {
+    }
+
+
+    protected void setStateLoginForm(Form f, Hashtable state) {
+    }
+
+
     protected void setStateMain(Form f, Hashtable state) {
     }
+
+    protected void handleComponentAction(Component c, ActionEvent event) {
+        Container rootContainerAncestor = getRootAncestor(c);
+        if(rootContainerAncestor == null) return;
+        String rootContainerName = rootContainerAncestor.getName();
+        Container leadParentContainer = c.getParent().getLeadParent();
+        if(leadParentContainer != null && leadParentContainer.getClass() != Container.class) {
+            c = c.getParent().getLeadParent();
+        }
+        if(rootContainerName == null) return;
+        if(rootContainerName.equals("RequestAccess")) {
+            if("Button".equals(c.getName())) {
+                onRequestAccess_ButtonAction(c, event);
+                return;
+            }
+            if("TextField".equals(c.getName())) {
+                onRequestAccess_TextFieldAction(c, event);
+                return;
+            }
+        }
+        if(rootContainerName.equals("Main")) {
+            if("Button1".equals(c.getName())) {
+                onMain_Button1Action(c, event);
+                return;
+            }
+        }
+    }
+
+      protected void onRequestAccess_ButtonAction(Component c, ActionEvent event) {
+      }
+
+      protected void onRequestAccess_TextFieldAction(Component c, ActionEvent event) {
+      }
+
+      protected void onMain_Button1Action(Component c, ActionEvent event) {
+      }
 
 }
--- smms/src/userclasses/StateMachine.java
+++ smms/src/userclasses/StateMachine.java
@@ -5,8 +5,6 @@
 package userclasses;
 
 import generated.StateMachineBase;
-import com.codename1.ui.*; 
-import com.codename1.ui.events.*;
 import com.codename1.ui.util.Resources;
 
 /**
@@ -23,8 +21,20 @@ public class StateMachine extends StateMachineBase {
     /**
      * this method should be used to initialize variables instead of
      * the constructor/class scope to avoid race conditions
+     * @param res
      */
+    @Override
     protected void initVars(Resources res) {
     }
 
+
+    protected boolean onRequestAccessSend() {
+        
+        return true;
+    }
+
+    protected boolean onMainRequestAccess() {
+        
+        return true;
+    }
 }
